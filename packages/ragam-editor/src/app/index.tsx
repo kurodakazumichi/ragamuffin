5566b6ab2a35c89f04587152f4e88288ffa5f399
kazumichi kuroda <kazumichi-kuroda@nekonecode.com>
2019-09-25 17:10:39 +0900

    #2 Updated from classic style to react style.

--- packages/ragam-editor/src/app/index.tsx
+++ packages/ragam-editor/src/app/index.tsx
@@ -1,1 +1,26 @@
-console.log("Hello World");
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+/**
+ * アプリケーション
+ */
+export default class App extends React.Component {
+
+  constructor(props:any) {
+    super(props);
+  }
+
+  render() {
+    
+    return (
+      <div className="app">
+        Hello World
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
