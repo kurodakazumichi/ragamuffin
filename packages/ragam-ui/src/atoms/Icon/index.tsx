d7625cc06a784ac1827a1361cf4e92836d2bb6c6
kazumichi kuroda <kazumichi-kuroda@nekonecode.com>
2019-09-26 20:04:17 +0900

    #11 Added Icon

---
+++ packages/ragam-ui/src/atoms/Icon/index.tsx
@@ -0,0 +1,115 @@
+/** @jsx jsx */
+/******************************************************************************
+ * import area
+ *****************************************************************************/
+import * as React from 'react';
+import ClassNames from 'classnames';
+import '@fortawesome/fontawesome-free/css/all.css'
+import { observer } from 'mobx-react';
+import { jsx, css } from '@emotion/core';
+
+/******************************************************************************
+ *  Enum
+ *****************************************************************************/
+/** Iconのスタイル */
+export enum Style {
+  /** 黒塗り */
+  Solid, 
+  /** 中抜き */
+  Regular,
+  /** ブランド */
+  Brand,
+}
+
+/** 
+ * Iconの種類
+ * font-awesomeのアイコン名を列挙型で定義する。
+ * https://fontawesome.com/icons?d=gallery&m=free
+ */
+export enum Type {
+  Eye        = "eye",
+  EyeSlash   = "eye-slash",
+  Play       = "play",
+  Stop       = "stop",
+  ArrowRight = "long-arrow-alt-right",
+  Square     = "square",
+  Image      = "image",
+}
+
+/** 
+ * 空のスタイル
+ */
+export const style = css({});
+
+/******************************************************************************
+ * 定数
+ *****************************************************************************/
+const StyleNames = {
+  [Style.Solid]  :"fas",
+  [Style.Regular]:"far",
+  [Style.Brand]  :"fab",
+};
+
+/******************************************************************************
+ * Interface
+ *****************************************************************************/
+/** Icon Props */
+export interface IProps {
+  /** 黒塗り、中抜き、ブランド系アイコンの指定 */
+  style?: Style, 
+  /** Iconの種類 */
+  type : Type,
+  /** クリック時の処理 */
+  onClick?:(event:React.MouseEvent<HTMLElement>) => void;
+};
+
+/******************************************************************************
+ * Icon Component
+ *****************************************************************************/
+@observer
+export default class Icon extends React.Component<IProps> 
+{
+  /** props規定値 */
+  static defaultProps:IProps = {
+    /** アイコンのスタイル */
+    style   :Style.Solid,
+    type    :Type.Eye,
+    onClick :undefined
+  }
+
+  /** コンストラクタ */
+  constructor(props:IProps) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  /** 描画 */
+  render() {
+    return (
+      <i css={style} className={this.className} onClick={this.onClick}/>
+    );
+  }
+
+  /** CSSクラス名 */
+  private get className() 
+  {
+    return ClassNames(
+      StyleNames[this.style],
+      `fa-${this.props.type}`,
+    );
+  }
+
+  /** 
+   * Iconの白抜き、黒抜きなどのスタイル
+   */
+  private get style():Style {
+    const {style} = this.props;
+    return (style)? style : Style.Solid;
+  }
+
+  /** クリック時の処理 */
+  private onClick(event:React.MouseEvent<HTMLElement>) {
+    const { onClick } = this.props;
+    onClick && onClick(event);
+  }
+}
---
+++ packages/ragam-ui/src/atoms/Icon/story/index.stories.tsx
@@ -0,0 +1,43 @@
+/******************************************************************************
+ * import
+ *****************************************************************************/
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { select } from '@storybook/addon-knobs';
+import * as Icon from '../';
+import { action } from '@storybook/addon-actions';
+import * as util from '@puyan/ts-util';
+
+/******************************************************************************
+ * 定数
+ *****************************************************************************/
+const types :any = util.enum2StringKeyObject(Icon.Type);
+const styles:any = util.enum2StringKeyObject(Icon.Style);
+
+/******************************************************************************
+ * story
+ *****************************************************************************/
+const stories = storiesOf('Core/Atoms', module);
+
+/**
+ * 通常のストーリー
+ */
+const defaultStory = () => {
+
+  // knobs-props.style
+  const style    = select(
+    "style",
+    styles, 
+    Icon.Style.Solid
+  );
+
+  // knobs-props.type
+  const type = select("type", types, Icon.Type.Eye)
+
+  // action
+  const onClick = action('onClick');
+  
+  const props = {style, type, onClick}
+  return <Icon.default {...props} />;
+}
+stories.add('Icon', defaultStory, {info:{propTables:[]}});
