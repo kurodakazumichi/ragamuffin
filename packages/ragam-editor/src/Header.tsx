ccb73fb6f58798b959a69153851d59de7ad93eeb
kazumichi kuroda <kazumichi-kuroda@nekonecode.com>
2019-09-28 12:13:36 +0900

    #24 Added Header test,

--- packages/ragam-editor/src/Header.tsx
+++ packages/ragam-editor/src/Header.tsx
@@ -4,7 +4,35 @@
  *****************************************************************************/
 import * as React from 'react';
 import { atoms, molecules }  from '@puyan/ragam-ui';
-import { jsx } from '@emotion/core';
+import { jsx, css } from '@emotion/core';
+
+/******************************************************************************
+ * style
+ *****************************************************************************/
+const style = {
+  wrapper: css({
+    display: "flex",
+    alignItems:"center",
+    backgroundColor:"#333",
+    padding: "5px 10px",
+  }),
+  title: css({
+    flexGrow:2,
+    "& > .a-inputText":{
+      color:"white",
+      width:"100%",
+    }
+  }),
+  player: css({
+    flexGrow:3,
+  }),
+  toolbox: css({
+    flexGrow:3,
+    "& > .m-buttonIcon": {
+      marginRight: "5px"
+    }
+  })
+}
 
 /******************************************************************************
  * Header
@@ -12,10 +40,14 @@ import { jsx } from '@emotion/core';
 export default class Header extends React.Component {
   render() {
     return (
-      <div>
-        <div><atoms.InputText.default value="タイトル" /></div>
-        <div>
+      <div css={style.wrapper}>
+        <div css={style.title}>
+          <atoms.InputText.default value="タイトル" />
+        </div>
+        <div css={style.player}>
           <molecules.ButtonIcon.default type={molecules.ButtonIcon.Type.Play}/>
+        </div>
+        <div css={style.toolbox}>
           <molecules.ButtonIcon.default type={molecules.ButtonIcon.Type.Shape}/>
           <molecules.ButtonIcon.default type={molecules.ButtonIcon.Type.Image}/>
         </div>
